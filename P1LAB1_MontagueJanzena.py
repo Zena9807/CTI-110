4e2e493990818adc818c8f755151576742fd8c15
Zena9807 <janzena9807@gmail.com>
2026-09-21 20:30:55 -0400

    Add files via upload
    P1LAB1 Upload

---
+++ P1LAB1_MontagueJanzena.py
@@ -0,0 +1,9 @@
+# Janzena Montague
+# September 21, 2026
+# P1LAB1
+# This program asks the user for their first and last name and displays a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Welcome,", first_name, last_name + "!")
