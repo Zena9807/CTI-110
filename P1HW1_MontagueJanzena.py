070f3ec41b05c0da428136e762e40f7a6b1917c5
Zena9807 <janzena9807@gmail.com>
2026-09-21 22:49:56 -0400

    Add files via upload
    Adding P1HW1

---
+++ P1HW1_MontagueJanzena.py
@@ -0,0 +1,21 @@
+# Janzena Montague
+# September 21, 2026
+# P1HW1
+# This program performs calculations using numbers input by the user.
+
+print("-----Calculating Exponents-----")
+print()
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+result = base ** exponent
+print()
+print(base, "raised to the power of", exponent, "is", result, "!!")
+print()
+print("-----Addition and Subtraction-----")
+print()
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+answer = num1 + num2 - num3
+print()
+print(num1, "+", num2, "-", num3, "is equal to", answer)
